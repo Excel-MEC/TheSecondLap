efe10db8b7fc95cbf7ae0d260ea74dfe3a87c870
ashwinkk <ashwinkailas@gmail.com>
2017-08-08 15:57:27 +0530

    merged branches

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -2658,22 +2658,23 @@ function calculateAndDisplayRoute(
 	);
 }
 
- $(document).ready(function(){
- 	var i=0;
- 	function LoopForever(){
- 		if (i%3==0) {
- 			$(".gold").delay(2000).fadeOut();
-     		$(".yesbank").delay(2000).fadeIn();
- 		}
- 		else if (i%3==1) {
- 			$(".yesbank").delay(2000).fadeOut();
- 			$(".medical").delay(2000).fadeIn();
- 		}
- 		else {
- 			$(".medical").delay(2000).fadeOut();
- 			$(".gold").delay(2000).fadeIn();
- 		}
- 		i++;
- 	}
- 	var interval = self.setInterval(function(){LoopForever()},3000);
- });
+$(document).ready(function() {
+	var i = 0;
+	function LoopForever() {
+		if (i == 0) $(".gold,.yesbank,.medical").fadeOut();
+		if (i % 3 == 0) {
+			$(".gold").fadeOut();
+			$(".yesbank").fadeIn();
+		} else if (i % 3 == 1) {
+			$(".yesbank").fadeOut();
+			$(".medical").fadeIn();
+		} else {
+			$(".medical").fadeOut();
+			$(".gold").fadeIn();
+		}
+		i++;
+	}
+	var interval = self.setInterval(function() {
+		LoopForever();
+	}, 3000);
+});
