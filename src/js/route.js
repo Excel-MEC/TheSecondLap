e343a30c422639af5baa824b788a89a7651a5963
ashwinkk <ashwinkailas@gmail.com>
2017-08-08 10:30:17 +0530

    map change

--- src/js/route.js
+++ src/js/route.js
@@ -4,30 +4,42 @@
 
 function initMap() {
 	var markerArray = [];
-	var directionsService = new google.maps.DirectionsService;
-	var map = new google.maps.Map(document.getElementById('map'),{
-		scrollwheel:false,
-		zoom:02,
-		center:{lat:9.9467,lng:76.2880}
+	var directionsService = new google.maps.DirectionsService();
+	var map = new google.maps.Map(document.getElementById("map"), {
+		scrollwheel: false,
+		zoom: 02,
+		center: { lat: 9.9467, lng: 76.288 }
 	});
-	var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+	var directionsDisplay = new google.maps.DirectionsRenderer({ map: map });
 	calculateAndDisplayRoute(
-		directionsDisplay, directionsService, markerArray,map);
+		directionsDisplay,
+		directionsService,
+		markerArray,
+		map
+	);
 }
-function calculateAndDisplayRoute(directionsDisplay, directionsService,
-                                  markerArray, map) {
-	directionsService.route({
-		origin: 'Cochin Port Trust Stadium,Bristow Rd Willingdon Island Kochi Kerala',
-		destination:'Marine Drive Ground Ernakulam Kerala',
-		travelMode: 'WALKING'
-	}, function(response, status) {
-		// Route the directions and pass the response to a function to create
-		// markers for each step.
-		console.log(response)
-		if (status === 'OK') {
-			directionsDisplay.setDirections(response);
-		} else {
-			console.log("error in google maps")
+function calculateAndDisplayRoute(
+	directionsDisplay,
+	directionsService,
+	markerArray,
+	map
+) {
+	directionsService.route(
+		{
+			origin:
+				"Cochin Port Trust Stadium,Bristow Rd Willingdon Island Kochi Kerala",
+			destination: "Marine Drive Ground Ernakulam Kerala",
+			travelMode: "DRIVING"
+		},
+		function(response, status) {
+			// Route the directions and pass the response to a function to create
+			// markers for each step.
+			console.log(response);
+			if (status === "OK") {
+				directionsDisplay.setDirections(response);
+			} else {
+				console.log("error in google maps");
+			}
 		}
-	});
+	);
 }
